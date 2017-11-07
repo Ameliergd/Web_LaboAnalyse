3495eae4c9d84fe588fe3076d2e03e029e314e1e
H4nks <tlenfant@yahoo.fr>
2017-11-07 11:37:06 +0100

    Style

--- connexionPatient.php
+++ connexionPatient.php
@@ -46,7 +46,7 @@ if (!empty($_POST['loginSubmit'])) {
 </head>
 
 <body>
-<section class="hero is-success is-fullheight">
+<section class="hero is-light is-fullheight">
     <div class="hero-body">
         <div class="container has-text-centered">
             <div class="column is-4 is-offset-4">
