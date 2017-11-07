ca4c9112a44daf1723d9d7c769406505e0d03882
Unknown <pierre.lourdelle@hei.yncrea.fr>
2017-11-07 16:55:17 +0100

    modif page article

---
+++ LearnMoreDepistage.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Cover - Free Bulma template</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <!-- Bulma Version 0.6.0 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous" />
+  <style type="text/css">
+    html,body {
+      font-family: 'Open Sans';
+    }
+  </style>
+</head>
+<body>
+  <section class="hero is-fullheight is-default is-bold">
+  <div class="hero-head">
+    <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                        <img src="/images/logo.png" alt="Logo">
+                    </a>
+                    <a class="navbar-burger" data-target="navbarMenu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+
+
+                <div id="navbarMenu" class="navbar-menu">
+
+                    <div class="navbar-start">
+
+                            <a class="navbar-item" href="index.html">ACCUEIL</a>
+                            <a class="navbar-item" href="informations.html">INFORMATIONS</a>
+                    </div>
+                    <div class="navbar-end is-grouped">
+
+                        <div class="navbar-item control">
+                            <a class="button is-primary" href="patients.php" target="_blank">
+                                  <span class="icon">
+                                      <i class="fa fa-user" aria-hidden="true"></i>
+                                  </span>
+                                <span>ESPACE CLIENT</span>
+                            </a>
+                        </div>
+                        <div class="navbar-item control">
+                            <a class="button is-info" href="medecin.php" target="_blank">
+                                  <span class="icon">
+                                      <i class="fa fa-user-md" aria-hidden="true"></i>
+                                  </span>
+                                <span>ESPACE MEDECIN</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+      </nav>
+  </div>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="columns is-vcentered">
+          <div class="column is-5">
+            <figure class="image is-4by3">
+              <img src="/images/depister-cancer.jpg" alt="Description">
+            </figure>
+          </div>
+          <div class="column is-6 is-offset-1">
+            <h1 class="title is-2">
+              L’IMPORTANCE DU DÉPISTAGE DES MALADIES
+            </h1>
+            <h2 class="subtitle is-4">
+              Le dépistage des maladies est un processus complexe et son efficacité dépend de multiples conditions. C’est le médecin, hospitalier ou ambulatoire, de premier recours ou spécialiste, qui est souvent en première ligne pour affronter cette complexité.
+            </h2>
+            <br>
+            <p class="has-text-centered">
+Identifier plus tôt une maladie pour intervenir vite et améliorer son pronostic est une approche facile à comprendre. Lorsqu’il est appliqué aux individus asymptomatiques, c’est-à-dire qui ne montrent aucun symptôme, cela devient du dépistage ; geste complexe de prévention secondaire.
+
+Un test de dépistage est en général simple. En revanche, le programme de dépistage dans la population est une intervention complexe, qui va du recrutement des personnes à dépister jusqu’à la prise en charge des malades. C’est un triple processus. Le premier a pour but de déterminer la probabilité qu’un individu présente une condition (maladie, prémices ou facteurs de risque d’une maladie). Le second vise à établir un diagnostic. Et le troisième consiste à intervenir en cas de diagnostic positif, dans le but de soigner efficacement (intervention médicale ou chirurgicale) ou de surveiller l’évolution de la pathologie.<br/><br/>
+
+Phase avant l’apparition de symptômes (phase préclinique)<br/>
+
+Ne sont concernés par un dépistage que les individus qui sont en phase préclinique, pendant laquelle la maladie est détectable mais ne se traduit pas encore par des symptômes. Toutes les maladies présentent une phase préclinique. Mais le dépistage ne peut être utile que pour les maladies dont cette phase est suffisamment longue, comme c’est le cas de la plupart des cancers et des maladies cardiovasculaires.
+
+Les cas diagnostiqués dans le cadre d’un dépistage ne sont pas identiques aux cas diagnostiqués à la suite de l’apparition de symptômes. Les différences portent autant sur l’histoire naturelle de la maladie (p.ex. vitesse de progression) que sur la réponse aux traitements. Une conséquence importante pour le médecin est que les connaissances médicales disponibles pour un type de cas ne sont pas forcément applicables à l’autre type.
+
+Lors d’un dépistage, le processus pathologique est généralement diagnostiqué à un stade plus précoce qu’en l’absence de dépistage. Par exemple, les participants à un dépistage par scanner à faible dose (c’est-à-dire qui irradie moins que d’autres appareils) qui voient en moyenne leurs cancers du poumon diagnostiqués environ quatre ans avant les cancers des non-participants au dépistage, présentent également des lésions moins avancées que les patients n’ayant pas participé à un dépistage.<br/><br/>
+
+Les tests de dépistage<br/>
+
+Les caractéristiques du test de dépistage sont aussi importantes que celles de la maladie. Les tests de dépistages  se distinguent des tests diagnostiques d’au moins deux façons.
+
+Premièrement, parce que l’objectif d’un test de dépistage est d’estimer la probabilité d’une maladie, et non pas de déterminer définitivement la présence ou l’absence d’une maladie (ce qui est le but d’un test diagnostique), les tests de dépistages sont moins performants que les tests diagnostiques. A quelques exceptions près, la plupart des programmes de dépistage opèrent donc en deux temps, avec une première phase de tri (test de dépistage) sur une large population, suivie d’une seconde phase diagnostique (test diagnostique) réservée aux participants  ayant un test de dépistage positif.
+
+Une deuxième différence est que les tests de dépistage sont généralement conçus pour s’appliquer qu’aux individus sans symptômes. En revanche, les tests diagnostiques peuvent s’appliquer aux individus avec et sans symptômes.<br/><br/>
+
+Adhérence au programme<br/>
+
+Parce que la population-cible d’un programme de dépistage est constituée d’individus ne présentant pas de symptômes, leurs participations au programme de soins comme une invitation à un dépistage  est en général plus  faible que la participation aux soins de  patients qui souffrent de symptômes. C’est pourquoi, le dépistage comporte  d’importantes procédures d’invitations et de rappels de façon à les convaincre de participer et de maintenir une bonne participation au programme.
+
+Pour être efficace, la détection précoce d’une maladie doit être suivie d’une procédure diagnostique, puis, le cas échéant, d’une intervention appropriée et efficace. Il faut en particulier que tous les cas diagnostiqués puissent bénéficier de l’intervention efficace dans des délais raisonnables.
+
+D’une façon générale, parce que le dépistage s’adresse à des individus qui n’ont rien demandé, l’organisateur doit d’abord informer précisément le participant des risques et des bénéfices du programme de dépistage,  et ceci du test de dépistage jusqu’à l’éventuelle intervention; il doit s’assurer du suivi des participants testés positifs au dépistage et, enfin, il doit soutenir les individus qui développent la maladie malgré un test négatif lors du dépistage (faux négatif).
+
+La façon de mesurer l’efficacité doit être définie initialement et dépend de l’objectif du dépistage (réduction de la mortalité, réduction de la morbidité, réduction des coûts, etc.). Par exemple, l’objectif initial d’un programme de dépistage du cancer du poumon est la réduction de la mortalité par cancer du poumon.
+
+Durant les décennies à venir et compte tenu des nouvelles technologies, le domaine des dépistages verra son importance croître, avec de nouvelles propositions de maladies à dépister, de nouveaux tests facilement accessibles par le grand public (tests génétiques sur internet par exemple). Il faudra continuer d’assurer la qualité des programmes de dépistages et du partage de l’information, notamment en utilisant de nouvelles techniques de décision pour le participant (technique de la décision partagée entre le médecin et le patient par exemple).<br/><br/>
+
+                Références <br/>
+
+ Adapté de « Dépistage : principes et méthodes », Dr Idris Guessous, Pr Jean-Michel Gaspoz, Unité d’épidémiologie populationnelle, Service de médecine de premier recours, Département de médecine communautaire et de premier recours, HUG ; Dr Idris Guessous, Pr Fred Paccaud, Unité de prévention communautaire, Institut universitaire de médecine sociale et préventive, Université de Lausanne ; Pr Jacques Cornuz PMU, Lausanne, in Revue médicale suisse 2010 ;6 :1390-4, en collaboration avec les auteurs
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script async type="text/javascript" src="../js/bulma.js"></script>
+</body>
+</html>
