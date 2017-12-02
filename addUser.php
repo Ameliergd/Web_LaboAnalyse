6832c5cdeeddc72e05fa9fdac4128688ce9832f6
H4nks <tlenfant@yahoo.fr>
2017-12-02 12:59:35 +0100

    Design

--- addUser.php
+++ addUser.php
@@ -22,37 +22,35 @@ if (!empty($_POST['adduserform'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
+  <!DOCTYPE html>
+  <html>
 
-<head>
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ajouter Patient</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <!-- Bulma Version 0.6.0 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css"
-          integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous"/>
-</head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous" />
+  </head>
 
-<body>
+  <body>
 
 
-  <nav class="navbar">
+    <nav class="navbar">
       <div class="container">
         <div class="navbar-brand">
-            <a class="navbar-item" href="index.html">
+          <a class="navbar-item" href="index.html">
                 <img src="images/logo.png" alt="Logo">
             </a>
-            <div class="navbar-item">
-                <p>Espace Médecin</p>
-            </div>
-            <a class="navbar-burger" data-target="navbarMenu">
+          <div class="navbar-item">
+            <p>Espace Médecin</p>
+          </div>
+          <a class="navbar-burger" data-target="navbarMenu">
                 <span></span>
                 <span></span>
                 <span></span>
@@ -60,146 +58,149 @@ if (!empty($_POST['adduserform'])) {
         </div>
         <div id="navbarMenu" class="navbar-menu">
           <div class="navbar-end">
-              <div class="navbar-item">
-                  <a class="button is-danger" href="medecin.php">
+            <div class="navbar-item">
+              <a class="button is-danger" href="medecin.php">
                       Logout
                   </a>
-              </div>
+            </div>
           </div>
+        </div>
       </div>
-      </div>
-  </nav>
+    </nav>
 
-<div class="hero is-success is-bold">
-    <div class="hero-body container has-text-centered">
+    <div class="hero is-success is-bold">
+      <div class="hero-body container has-text-centered">
         <h1 class="title">
             Ajouter un nouveau patient.
         </h1>
+      </div>
     </div>
-</div>
-<div class="section container">
-    <div class="columns">
+    <div class="section container">
+      <div class="columns">
         <div class="column is-7 is-offset-2">
-            <form method="post" action="">
+          <form method="post" action="">
 
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">Patient</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <p class="control is-expanded">
-                                <input class="input" type="text" placeholder="Nom" name="nom" required>
-                            </p>
-                        </div>
-                        <div class="field">
-                            <p class="control is-expanded">
-                                <input class="input" type="text" placeholder="Prénom" name="prenom" required>
-                            </p>
-                        </div>
-                    </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Patient</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded">
+                    <input class="input" type="text" placeholder="Nom" name="nom" required>
+                  </p>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">Email</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <p class="control is-expanded has-icons-left">
-                                <input class="input" type="text" placeholder="mail@mail.com" name="email" required>
-                                <span class="icon"><i class="fa fa-envelope" aria-hidden="true"></i>
+                <div class="field">
+                  <p class="control is-expanded">
+                    <input class="input" type="text" placeholder="Prénom" name="prenom" required>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Email</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="mail@mail.com" name="email" required>
+                    <span class="icon is-left"><i class="fa fa-envelope" aria-hidden="true"></i>
 </span>
-                            </p>
-                        </div>
-                    </div>
+                  </p>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">Mot de Passe</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <p class="control is-expanded has-icons-left">
-                                <input class="input" type="password" placeholder="*******" name="password" required>
-                                <span class="icon"><i class="fa fa-key" aria-hidden="true"></i>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Mot de Passe</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="password" placeholder="*******" name="password" required>
+                    <span class="icon is-left"><i class="fa fa-key" aria-hidden="true"></i>
 </span>
-                            </p>
-                        </div>
-                    </div>
+                  </p>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">Sexe</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field is-narrow">
-                            <div class="control">
-                                <label class="radio">
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Sexe</label>
+              </div>
+              <div class="field-body">
+                <div class="field is-narrow">
+                  <div class="control">
+                    <label class="radio">
                                     <input type="radio" name="sexe" value="M">
-                                    Masculin
+                                    <span class="icon" style="color: pink;"><i class="fa fa-venus fa-lg" aria-hidden="true"></i>
+                                    </span>
                                 </label>
-                                <label class="radio">
+                    <label class="radio">
                                     <input type="radio" name="sexe" value="F">
-                                    Féminin
+                                    <span class="icon" style="color: cyan;"><i class="fa fa-mars fa-lg" aria-hidden="true"></i>
+</span>
                                 </label>
-                            </div>
-                        </div>
-                    </div>
+                  </div>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">Âge</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <p class="control">
-                                <input class="input" type="number" name="age" required>
-
-                            </p>
-                        </div>
-                    </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Âge</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input class="input" type="number" name="age" required>
+                    <span class="icon is-left"><i class="fa fa-calendar" aria-hidden="true"></i>
+</span>
+                  </p>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <label class="label">N° Sécu</label>
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <p class="control has-icons-left">
-                                <input class="input" type="text" name="numsecu" required>
-                                <span class="icon">
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">N° Sécu</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input class="input" type="text" name="numsecu" required>
+                    <span class="icon is-left">
                                 <i class="fa fa-id-card-o" aria-hidden="true"></i>
 
                             </span>
 
-                            </p>
-                        </div>
-                    </div>
+                  </p>
                 </div>
-                <div class="field is-horizontal">
-                    <div class="field-label">
-                        <!-- Left empty for spacing -->
-                    </div>
-                    <div class="field-body">
-                        <div class="field">
-                            <div class="control is-pulled-right">
-                                <input type="submit" class="button is-success" name="adduserform">
-                                <a class="button is-danger" href="patients.php">
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <!-- Left empty for spacing -->
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control is-pulled-right">
+                    <input type="submit" class="button is-success" name="adduserform">
+                    <a class="button is-danger" href="patients.php">
                                     Annuler
                                 </a>
-                            </div>
-                        </div>
-                    </div>
+                  </div>
                 </div>
+              </div>
+            </div>
 
 
-            </form>
+          </form>
         </div>
+      </div>
+
     </div>
 
-</div>
+    <script src="js/navbarburger.js"></script>
+  </body>
 
-<script src="js/navbarburger.js"></script>
-</body>
-
-</html>
+  </html>
